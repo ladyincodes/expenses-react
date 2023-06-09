bdc7e2e28fdd0d60af9a2f7ad8576e855cb46461
Ati <eskafiati@gmail.com>
2023-06-10 00:01:12 +1200

    Seperated Card component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
@@ -28,26 +28,7 @@ function App() {
   return (
     <div className='App'>
       <h2>Let's get it started!</h2>
-      <ExpenseItem
-        date={expenses[0].date}
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-      />
-      <ExpenseItem
-        date={expenses[1].date}
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-      />
-      <ExpenseItem
-        date={expenses[2].date}
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-      />
-      <ExpenseItem
-        date={expenses[3].date}
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-      />
+      <Expenses items={expenses} />
     </div>
   );
 }
